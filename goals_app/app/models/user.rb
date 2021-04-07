7afa32a8cb8a27a1913cf40d2b128ae0cc6bad32
your name here <abirmahamud610@gmail.com>
2021-04-07 17:35:25 -0400

    Final Commit

--- goals_app/app/models/user.rb
+++ goals_app/app/models/user.rb
@@ -9,8 +9,16 @@
 #
 class User < ApplicationRecord
 
-    validates :username, :session_token, :password_digest, presence: true 
+    validates :username, presence: true 
     validates :username, uniqueness: true 
     # validates :password, length: {minimum: 6, allow_nil: true}
     
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+
 end
--- goals_app/spec/models/user_spec.rb
+++ goals_app/spec/models/user_spec.rb
@@ -18,25 +18,25 @@ RSpec.describe User, type: :model do
   
     subject(:user) {FactoryBot.build(:user)}
 
-    describe "validations"  do 
+    describe 'validations'  do 
         it { should validate_presence_of(:username)}
         it { should validate_presence_of(:session_token)}
         it { should validate_presence_of(:password_digest)}
         it { should validate_uniqueness_of(:username)}
         it { should validate_uniqueness_of(:session_token)}
-        it {should validate_length_of(:password).is_at_least(6) }
+        it { should validate_length_of(:password).is_at_least(6) }
     end
 
-    describe "class methods" do
+    describe 'class methods' do
         
         describe '::find_by_credentials' do 
-            context "with a valid username and password" do
+            context 'with a valid username and password' do
 
                 it 'should return correct user' do 
                     expect(User.find_by_credentials).to eq(:user.username)
                 end
             end 
-            context "with an invalid username" do 
+            context 'with an invalid username' do 
                 it 'should return nil' do 
                     expect(User.find_by_credentials).to eq(nil)
                 end
@@ -45,8 +45,8 @@ RSpec.describe User, type: :model do
 
     end
 
-    describe "password methods" do 
-        describe "it does not save passwords to the database" do
+    describe 'password methods' do 
+        describe 'it does not save passwords to the database' do
             it ' saves passwords properly' do 
                 expect(BCrypt::Password.create('password')).to eq(:password_digest)
             end
@@ -56,12 +56,12 @@ RSpec.describe User, type: :model do
         end
     end
 
-    describe "session token methods" do
+    describe 'session token methods' do
         it 'assigns a session token if one is not given' do
             expect(user.session_token).not_to be_nil 
         end
 
-        it "resets the session token on the user" do 
+        it 'resets the session token on the user' do 
             old_token = user.session_token 
             new_token = user.reset_session_token!
             expect(old_token).not_to eq(new_token)
