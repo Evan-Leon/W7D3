8a861436f915f666eb674d682bf010e219ba7e3c
Evan-Leon <evan.leon.j@gmail.com>
2021-04-07 16:08:06 -0400

    Added further specs for user_model

--- goals_app/app/models/user.rb
+++ goals_app/app/models/user.rb
@@ -9,5 +9,8 @@
 #
 class User < ApplicationRecord
 
+    validates :username, :session_token, :password_digest, presence: true 
+    validates :username, uniqueness: true 
+    # validates :password, length: {minimum: 6, allow_nil: true}
     
 end
