03c17c6b0b759d33e9773e1a50c95ab2077224e5
Evan-Leon <evan.leon.j@gmail.com>
2021-04-07 15:14:47 -0400

    Added specs to our users model tests

--- goals_app/db/schema.rb
+++ goals_app/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_182857) do
+ActiveRecord::Schema.define(version: 2021_04_07_184458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "test_models", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
--- goals_app/spec/models/user_spec.rb
+++ goals_app/spec/models/user_spec.rb
@@ -13,6 +13,33 @@ RSpec.describe User, type: :model do
         it { should validate_presence_of(:username)}
         it { should validate_presence_of(:session_token)}
         it { should validate_presence_of(:password_digest)}
+        it { should validate_uniqueness_of(:username)}
+        it { should validate_uniqueness_of(:session_token)}
+        it {should validate_length_of(:password).is_at_least(6) }
+    end
+
+    describe "class methods" do
+        
+        describe '::find_by_credentials' do 
+            context "with a valid username and password" do
+
+                it 'should return correct user' do 
+                    expect(User.find_by_credentials).to eq(:user.username)
+                end
+            end 
+            context "with an invalid username" do 
+                it 'should return nil' do 
+                    expect(User.find_by_credentials).to eq(nil)
+                end
+            end
+        end
 
     end
+
+    describe "password methods"
+
+
+
+end
+
 end
